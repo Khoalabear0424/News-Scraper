bd9f648bbf7daebfbcd969d033cead5699eb7253
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-30 19:45:54 -0700

    return all article html elements

--- server.js
+++ server.js
@@ -27,8 +27,11 @@ app.get("/", function (req, res) {
 app.get("/scrape-page", function (req, res) {
     request('https://www.nytimes.com/', function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        res.send(body); // Print the HTML for the Google homepage.
+        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //console.log(body); // Print the HTML for the Google homepage.
+
+        const $ = cheerio.load(body);
+        console.log($('article'))
     });
 })
 
