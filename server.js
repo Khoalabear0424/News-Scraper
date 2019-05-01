f4f664c77cd4371e0a9bca69555b6fadd99f03d2
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-30 19:09:09 -0700

    set up server and console logging the body of ny times

---
+++ server.js
@@ -0,0 +1,134 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+    console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function (req, res) {
+    res.send("Hello world");
+});
+
+app.get("/scrape-page", function (req, res) {
+    request('https://www.nytimes.com/', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        res.send(body); // Print the HTML for the Google homepage.
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Retrieve data from the db
+app.get("/all", function (req, res) {
+    // Find all results from the scrapedData collection in the db
+    db.scrapedData.find({}, function (error, found) {
+        // Throw any errors to the console
+        if (error) {
+            console.log(error);
+        }
+        // If there are no errors, send the data to the browser as json
+        else {
+            res.json(found);
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+// app.post("/article", function (req, res) {
+//     db.scrapedData.insert({ name: "khoa" }, function (error, newArticle) {
+//         if (error) {
+//             console.log(error)
+//         } else {
+//             res.json(newArticle);
+//         }
+//     })
+// })
+
+
+
+
+
+
+
+
+
+
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function (req, res) {
+    // Make a request via axios for the news section of `ycombinator`
+    axios.get("https://news.ycombinator.com/").then(function (response) {
+        // Load the html body from axios into cheerio
+        var $ = cheerio.load(response.data);
+        // For each element with a "title" class
+        $(".title").each(function (i, element) {
+            // Save the text and href of each link enclosed in the current element
+            var title = $(element).children("a").text();
+            var link = $(element).children("a").attr("href");
+
+            // If this found element had both a title and a link
+            if (title && link) {
+                // Insert the data in the scrapedData db
+                db.scrapedData.insert({
+                    title: title,
+                    link: link
+                },
+                    function (err, inserted) {
+                        if (err) {
+                            // Log the error if one is encountered during the query
+                            console.log(err);
+                        }
+                        else {
+                            // Otherwise, log the inserted data
+                            console.log(inserted);
+                        }
+                    });
+            }
+        });
+    });
+
+    // Send a "Scrape Complete" message to the browser
+    res.send("Scrape Complete");
+});
+
+
+// Listen on port 3000
+app.listen(3000, function () {
+    console.log("App running on port 3000!");
+});
