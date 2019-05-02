58ac1195b2faa77ce4c1522b9ee4164ffb1874e5
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-01 22:19:00 -0700

    deleted somem commenets

--- server.js
+++ server.js
@@ -143,13 +143,10 @@ app.get("/all", function (req, res) {
 });
 
 app.get("/nordstrom", function (req, res) {
-    // Find all results from the scrapedData collection in the db
     db.scrapedDataNordStorm.find({}, function (error, found) {
-        // Throw any errors to the console
         if (error) {
             console.log(error);
         }
-        // If there are no errors, send the data to the browser as json
         else {
             res.json(found);
         }
